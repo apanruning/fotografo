d679061d250f41654c2c10e0db8c9a92dfcfb827
Matias Iturburu <maturburu@gmail.com>
2010-11-14 18:09:23 -0300

    added slugify function, will come handy in the future

---
+++ utils.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Modifies a string to remove al non ASCII characters and spaces.
+ */
+function slugify($text)
+{
+    // replace non letter or digits by -
+    $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+    // trim
+    $text = trim($text, '-');
+    // transliterate
+    if (function_exists('iconv')){
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    }
+    // lowercase
+    $text = strtolower($text);
+    // remove unwanted characters
+    $text = preg_replace('~[^-\w]+~', '', $text);
+    if (empty($text)){
+        return 'n-a';
+    }
+    return $text;
+}
+?>
