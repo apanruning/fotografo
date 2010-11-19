4298ff95660e8d53c20dad49d27b27a5cedf174c
Matias Iturburu <maturburu@gmail.com>
2010-11-19 19:50:56 -0300

    Corrected model schema

--- schema.sql
+++ schema.sql
@@ -1,15 +1,14 @@
-BEGIN;CREATE TABLE `modelbuilder_picture` (
+BEGIN;
+CREATE TABLE `picture` (
     `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `image` varchar(100) NOT NULL,
     `album_id` integer NOT NULL
-)
-;
-CREATE TABLE `modelbuilder_album` (
+);
+CREATE TABLE `album` (
     `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `name` varchar(60) NOT NULL,
     `slug` varchar(50) NOT NULL UNIQUE,
     `description` longtext NOT NULL,
     `created` datetime NOT NULL
-)
-;
-ALTER TABLE `modelbuilder_picture` ADD CONSTRAINT `album_id_refs_id_ccd3755a` FOREIGN KEY (`album_id`) REFERENCES `modelbuilder_album` (`id`);COMMIT;
+);
+ALTER TABLE `picture` ADD CONSTRAINT `album_id_refs_id_ccd3755a` FOREIGN KEY (`album_id`) REFERENCES `album` (`id`);COMMIT;
