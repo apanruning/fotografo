11b8c7f8636cfc26576a23b2657d08e8ed9a58dc
Matias Iturburu <maturburu@gmail.com>
2010-11-20 12:01:43 -0300

    Live image resizing

--- lib/ImageView.php
+++ lib/ImageView.php
@@ -1,6 +1,19 @@
 <?php 
 class ImageView extends View{
 
+	public static function rgb($int) {
+		$hex=str_pad(dechex($int),$int<4096?3:6,'0',STR_PAD_LEFT);
+		$len=strlen($hex);
+		if ($len>6) {
+			trigger_error(self::TEXT_Color);
+			return FALSE;
+		}
+		$color=str_split($hex,$len/3);
+		foreach ($color as &$hue)
+			$hue=hexdec(str_repeat($hue,6/$len));
+		return $color;
+	}
+	
     public static function thumb($file,$dimx,$dimy) {
 		preg_match('/\.(gif|jp[e]*g|png)$/',$file,$ext);
 		$ext[1]=str_replace('jpg','jpeg',$ext[1]);
@@ -23,18 +36,16 @@ class ImageView extends View{
 		}
 		// Create blank image
 		$tmp=imagecreatetruecolor($dimx,$dimy);
-		list($r,$g,$b)=self::rgb(self::$global['BGCOLOR']);
+		list($r,$g,$b)=self::rgb(0xFFF);
 		$bg=imagecolorallocate($tmp,$r,$g,$b);
 		imagefill($tmp,0,0,$bg);
 		// Resize
 		imagecopyresampled($tmp,$img,0,0,0,0,$dimx,$dimy,$oldx,$oldy);
 		// Make the background transparent
 		imagecolortransparent($tmp,$bg);
-		if (PHP_SAPI!='cli')
-			header('Content-Type: image/'.$ext[1]);
+		Slim::response()->header('Content-Type','image/'.$ext[1]);
 		// Send output in same graphics format as original
 		eval('image'.$ext[1].'($tmp);');
 	}
-
 }
 ?>
