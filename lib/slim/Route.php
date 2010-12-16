5ccc9e3d19cbc867791bf423dfeca0e8f987dfa9
Matias Iturburu <maturburu@gmail.com>
2010-12-16 03:33:10 -0300

    doh

---
+++ lib/slim/Route.php
@@ -0,0 +1,301 @@
+<?php
+/**
+ * Slim
+ *
+ * A simple PHP framework for PHP 5 or newer
+ *
+ * @author		Josh Lockhart <info@joshlockhart.com>
+ * @link		http://slim.joshlockhart.com
+ * @copyright	2010 Josh Lockhart
+ *
+ * MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * Slim Route
+ *
+ * @package	Slim
+ * @author	Josh Lockhart <info@joshlockhart.com>
+ * @since	Version 1.0
+ */
+class Route {
+
+	/**
+	 * @var string The route pattern (ie. "/books/:id")
+	 */
+	private $pattern;
+
+	/**
+	 * @var mixed The callable associated with this route
+	 */
+	private $callable;
+
+	/**
+	 * @var array Conditions for this route's URL parameters
+	 */
+	private $conditions = array();
+
+	/**
+	 * @var string The name of this route
+	 */
+	private $name;
+
+	/**
+	 * @var array Array of URL parameter names and values
+	 */
+	private $params = array();
+
+	/**
+	 * @var Router The Router that contains this Route
+	 */
+	private $router;
+
+	/**
+	 * Constructor
+	 *
+	 * @param string	$pattern	The URL pattern (ie. "/books/:id")
+	 * @param mixed		$callable	Anything that returns TRUE for is_callable()
+	 */
+	public function __construct( $pattern, $callable ) {
+		$this->setPattern(str_replace(')', ')?', $pattern));
+		$this->callable = $callable;
+	}
+
+	/***** ACCESSORS *****/
+	
+	/**
+	 * Get route pattern
+	 *
+	 * @return string
+	 */
+	public function getPattern() {
+		return $this->pattern;
+	}
+	
+	/**
+	 * Set route pattern
+	 *
+	 * @param string $pattern
+	 * @return void
+	 */
+	public function setPattern( $pattern ) {
+		$this->pattern = $pattern;
+	}
+	
+	/**
+	 * Get route callable
+	 *
+	 * @return mixed
+	 */
+	public function getCallable() {
+		return $this->callable;
+	}
+	
+	/**
+	 * Set route callable
+	 *
+	 * @param mixed $callable
+	 * @return void
+	 */
+	public function setCallable($callable) {
+		$this->callable = $callable;
+	}
+	
+	/**
+	 * Get route conditions
+	 *
+	 * @return array
+	 */
+	public function getConditions() {
+		return $this->conditions;
+	}
+	
+	/**
+	 * Set route conditions
+	 *
+	 * @param array $conditions
+	 * @return void
+	 */
+	public function setConditions( $conditions ) {
+		$this->conditions = (array)$conditions;
+	}
+	
+	/**
+	 * Get route name
+	 *
+	 * @return string|null
+	 */
+	public function getName() {
+		return $this->name;
+	}
+	
+	/**
+	 * Set route name
+	 *
+	 * @param string $name
+	 * @return void
+	 */
+	public function setName( $name ) {
+		$this->name = $name;
+		$this->getRouter()->cacheNamedRoute($name, $this);
+	}
+	
+	/**
+	 * Get route parameters
+	 *
+	 * @return array
+	 */
+	public function getParams() {
+		return $this->params;
+	}
+	
+	/**
+	 * Get router
+	 *
+	 * @return Router
+	 */
+	public function getRouter() {
+		return $this->router;
+	}
+	
+	/**
+	 * Set router
+	 *
+	 * @param Router $router
+	 * @return void
+	 */
+	public function setRouter( Router $router ) {
+		$this->router = $router;
+	}
+	
+	/***** ROUTE PARSING AND MATCHING *****/
+	
+	/**
+	 * Matches URI?
+	 *
+	 * Parse this route's pattern, and then compare it to an HTTP resource URI
+	 * This method was modeled after the techniques demonstrated by Dan Sosedoff at:
+	 *
+	 * http://blog.sosedoff.com/2009/09/20/rails-like-php-url-router/
+	 *
+	 * This method is also smart about trailing slashes. If a route is defined
+	 * with a trailing slash, and if the current request URI does not have
+	 * a trailing slash but otherwise matches the route, a SlimRequestSlashException
+	 * will be thrown triggering a 301 Redirect to the same URI with a trailing slash.
+	 * This exception is caught in the `Slim::run` loop. If a route is
+	 * defined without a trailing slash, and the current request URI does
+	 * have a trailing slash, the route will not be matched and a 404 Not Found
+	 * response will be sent if no subsequent matching routes are found.
+	 *
+	 * @param	string						$resourceUri A Request URI
+	 * @throws	SlimRequestSlashException
+	 * @return	bool
+	 */
+	public function matches( $resourceUri ) {
+
+		//Extract URL params
+		preg_match_all('@:([\w]+)@', $this->getPattern(), $paramNames, PREG_PATTERN_ORDER);
+		$paramNames = $paramNames[0];
+
+		//Convert URL params into regex patterns, construct a regex for this route
+		$patternAsRegex = preg_replace_callback('@:[\w]+@', array($this, 'convertPatternToRegex'), $this->getPattern());
+		if ( substr($this->getPattern(), -1) === '/' ) {
+			$patternAsRegex = $patternAsRegex . '?';
+		}
+		$patternAsRegex = '@^' . $patternAsRegex . '$@';
+
+		//Cache URL params' names and values if this route matches the current HTTP request
+		if ( preg_match($patternAsRegex, $resourceUri, $paramValues) ) {
+			array_shift($paramValues);
+			foreach ( $paramNames as $index => $value ) {
+				if ( isset($paramValues[substr($value, 1)]) ) {
+					$this->params[substr($value, 1)] = urldecode($paramValues[substr($value, 1)]);
+				}
+			}
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	/**
+	 * Convert a URL parameter (ie. ":id") into a regular expression
+	 *
+	 * @param	array 	URL parameters
+	 * @return 	string	Regular expression for URL parameter
+	 */
+	private function convertPatternToRegex( $matches ) {
+		$key = str_replace(':', '', $matches[0]);
+		if ( array_key_exists($key, $this->conditions) ) {
+			return '(?P<' . $key . '>' . $this->conditions[$key] . ')';
+		} else {
+			return '(?P<' . $key . '>[a-zA-Z0-9_\-\.\!\~\*\\\'\(\)\:\@\&\=\$\+,%]+)';
+		}
+	}
+
+	/***** HELPERS *****/
+	
+	/**
+	 * Set route name (alias for Route::setName)
+	 *
+	 * @param	string $name The name of the route
+	 * @return 	Route
+	 */
+	public function name( $name ) {
+		$this->setName($name);
+		return $this;
+	}
+
+	/**
+	 * Set route conditions (alias for Route::setConditions)
+	 *
+	 * @param	array $conditions An associative array of URL parameter conditions
+	 * @return 	Route
+	 */
+	public function conditions( $conditions ) {
+		if ( is_array($conditions) ) {
+			$this->conditions = $conditions;
+		}
+		return $this;
+	}
+	
+	/***** DISPATCHING *****/
+	
+	/**
+	 * Dispatch route
+	 *
+	 * @return bool
+	 */
+	public function dispatch() {
+		if ( substr($this->getPattern(), -1) === '/' && substr($this->getRouter()->getRequest()->resource, -1) !== '/' ) {
+			throw new SlimRequestSlashException();
+		}
+		if ( is_callable($this->getCallable()) ) {
+			call_user_func_array($this->getCallable(), array_values($this->getParams()));
+			return true;
+		}
+		return false;
+	}
+
+}
+?>
---
+++ lib/slim/Router.php
@@ -0,0 +1,250 @@
+<?php
+/**
+ * Slim
+ *
+ * A simple PHP framework for PHP 5 or newer
+ *
+ * @author		Josh Lockhart <info@joshlockhart.com>
+ * @link		http://slim.joshlockhart.com
+ * @copyright	2010 Josh Lockhart
+ *
+ * MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * Slim Router
+ *
+ * The Router is responsible for registering route paths with associated
+ * callback functions. When a Slim application is run, the Router is then
+ * responsible for matching a registered route with the current HTTP request,
+ * and if a matching route is found, executing the associated callback function
+ * using any URL parameters in the request URI.
+ *
+ * @package	Slim
+ * @author	Josh Lockhart <info@joshlockhart.com>
+ * @since	Version 1.0
+ */
+class Router implements Iterator {
+
+	/**
+	 * @var Request
+	 */
+	private $request;
+
+	/**
+	 * @var array Lookup hash of routes, keyed by Request method
+	 */
+	private $routes;
+
+	/**
+	 * @var array Lookup hash of named routes, keyed by route name
+	 */
+	private $namedRoutes;
+
+	/**
+	 * @var array Array of routes matching the Request method and URL
+	 */
+	private $matchedRoutes;
+	
+	/**
+	 * @var mixed 404 Not Found callback function if a matching route is not found
+	 */
+	private $notFound;
+
+	/**
+	 * @var mixed Error callback function if there is an application error
+	 */
+	private $error;
+
+	/**
+	 * @var int Iterator position
+	 */
+	private $position;
+	
+	/**
+	 * Constructor
+	 *
+	 * @param Request $request The current HTTP request object
+	 */
+	public function __construct( Request $request ) {
+		$this->request = $request;
+		$this->routes = array(
+			'GET' => array(),
+			'POST' => array(),
+			'PUT' => array(),
+			'DELETE' => array()
+		);
+		$this->position = 0;
+	}
+	
+	/***** ACCESSORS *****/
+	
+	/**
+	 * Get Request
+	 *
+	 * @return Request
+	 */
+	public function getRequest() {
+		return $this->request;
+	}
+	
+	/**
+	 * Set Request
+	 *
+	 * @param Request
+	 * @return void
+	 */
+	public function setRequest( Request $req ) {
+		$this->request = $req;
+	}
+	
+	/***** MAPPING *****/
+
+	/**
+	 * Map a route to a callback function
+	 *
+	 * @param	string	$pattern	The URL pattern (ie. "/books/:id")
+	 * @param	mixed	$callable	Anything that returns TRUE for is_callable()
+	 * @param	string	$method		The HTTP request method (GET, POST, PUT, DELETE)
+	 * @return 	Route
+	 */
+	public function map( $pattern, $callable, $method ) {
+		$route = new Route($pattern, $callable);
+		$route->setRouter($this);
+		$this->routes[$method][] = $route;
+		if ( $method === $this->getRequest()->method && $route->matches($this->getRequest()->resource) ) {
+			$this->matchedRoutes[] = $route;
+		}
+		return $route;
+	}
+
+	/**
+	 * Cache named route
+	 *
+	 * @param	string				$name	The route name
+	 * @param	Route				$route	The route object
+	 * @throws	RuntimeException			If a named route already exists with the same name
+	 * @return 	void
+	 */
+	public function cacheNamedRoute( $name, Route $route ) {
+		if ( isset($this->namedRoutes[(string)$name]) ) {
+			throw new RuntimeException('Named route already exists with name: ' . $name);
+		}
+		$this->namedRoutes[$name] = $route;
+	}
+
+	/**
+	 * Get URL for named route
+	 *
+	 * @param	string				$name	The name of the route
+	 * @param	array 						Associative array of URL parameter names and values
+	 * @throws	RuntimeException			If named route not found
+	 * @return 	string						The URL for the given route populated with the given parameters
+	 */
+	public function urlFor( $name, $params = array() ) {
+		if ( !isset($this->namedRoutes[(string)$name]) ) {
+			throw new RuntimeException('Named route not found for name: ' . $name);
+		}
+		$pattern = $this->namedRoutes[(string)$name]->getPattern();
+		foreach ( $params as $key => $value ) {
+			$pattern = str_replace(':' . $key, $value, $pattern);
+		}
+		return $this->getRequest()->root . $pattern;
+	}
+
+	/**
+	 * Register a 404 Not Found callback
+	 *
+	 * @param	mixed $callable Anything that returns TRUE for is_callable()
+	 * @return 	mixed
+	 */
+	public function notFound( $callable = null ) {
+		if ( is_callable($callable) ) {
+			$this->notFound = $callable;
+		}
+		return $this->notFound;
+	}
+
+	/**
+	 * Register a 500 Error callback
+	 *
+	 * @param	mixed $callable Anything that returns TRUE for is_callable()
+	 * @return 	mixed
+	 */
+	public function error( $callable = null ) {
+		if ( is_callable($callable) ) {
+			$this->error = $callable;
+		}
+		return $this->error;
+	}
+
+	/***** ITERATOR INTERFACE *****/
+
+	/**
+	 * Return the current route being dispatched
+	 *
+	 * @return Route
+	 */
+	public function current() {
+		return $this->matchedRoutes[$this->position];
+	}
+
+	/**
+	 * Reset the current route to the first matching route
+	 *
+	 * @return void
+	 */
+	public function rewind() {
+		$this->position = 0;
+	}
+
+	/**
+	 * Return the 0-indexed position of the current route 
+	 * being dispatched among all matching routes
+	 *
+	 * @return int
+	 */
+	public function key() {
+		return $this->position;
+	}
+
+	/**
+	 * Return the 0-indexed position of the next route to 
+	 * be dispatched among all matching routes
+	 *
+	 * @return int
+	 */
+	public function next() {
+		$this->position = $this->position + 1;
+	}
+
+	/**
+	 * Does a matching route exist at a given 0-indexed position?
+	 *
+	 * @return bool
+	 */
+	public function valid() {
+		return isset($this->matchedRoutes[$this->position]);
+	}
+
+}
+?>
